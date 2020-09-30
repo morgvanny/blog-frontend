36e2d48fd4a3bad5c09b09cba493189e87fb5463
Morgan VanYperen <morgvanny@gmail.com>
2020-09-30 08:03:39 -0700

    example code

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ fetch("http://localhost:3000/posts")
     });
   });
 
-const createPost = (postInfo) => {
-  return fetch("http://localhost:3000/posts", {
+const createPost = (postInfo, form) => {
+  fetchPromise = fetch("http://localhost:3000/posts", {
     method: "post",
     headers: {
       "Content-Type": "application/json",
@@ -41,23 +41,100 @@ const createPost = (postInfo) => {
   })
     .then((r) => r.json())
     .then((info) => {
-      const u = info.included[0];
-      renderPost(info.data, u);
+      if (info.errors) {
+        console.log(info.errors);
+      } else {
+        form.reset();
+        const u = info.included[0];
+        renderPost(info.data, u);
+      }
     });
 };
 
 const handleForm = (e) => {
-  const { title, content } = e.target;
   e.preventDefault();
-  createPost({
-    post: {
-      title: title.value,
-      content: content.value,
-      username: username.value,
+  createPost(
+    {
+      post: {
+        title: title.value,
+        content: content.value,
+        username: username.value,
+      },
     },
-  }).then(() => {
-    e.target.reset();
-  });
+    e.target
+  );
 };
 
 document.querySelector("form").addEventListener("submit", handleForm);
+
+const wes = {
+  first: "Wes",
+  last: "Bos",
+  links: {
+    social: {
+      twitter: "https://twitter.com/wesbos",
+      facebook: "https://facebook.com/wesbos.developer",
+    },
+    web: {
+      blog: "https://wesbos.com",
+    },
+  },
+};
+
+const nameLogger = ({ first = "Morgan", last }) => {
+  console.log(first);
+  console.log(last);
+};
+
+// nameLogger(wes);
+
+function individualNameLogger(first, last) {
+  console.log(first);
+  console.log(last);
+}
+
+const array = [5, 4, 3, 2, 1, ["b", "c", "a"]];
+
+const newArray = [...array.slice(0, 5), [...array[5]]];
+
+const newWes = {
+  ...wes,
+  links: { social: { ...wes.links.social }, web: { ...wes.links.web } },
+};
+
+const names = { first: "Wes", last: "Bos" };
+
+const links = {
+  first: "Morgan",
+  social: {
+    twitter: "https://twitter.com/wesbos",
+    facebook: "https://facebook.com/wesbos.developer",
+  },
+  web: {
+    blog: "https://wesbos.com",
+  },
+};
+
+const person = { ...names, ...links };
+
+const thing = false;
+
+thing ? console.log("thing was true") : console.log("thing was not true");
+
+const name = thing ? "Morgan" : "Wes";
+
+const num1 = 1;
+const num2 = 2;
+const num3 = 3;
+
+function add(...rest) {
+  let sum = 0;
+  rest.forEach((num) => {
+    sum += num;
+  });
+  return sum;
+}
+
+sum = add(num1, num2, num3, 15, 22, 33, 11);
+
+console.log(sum);
